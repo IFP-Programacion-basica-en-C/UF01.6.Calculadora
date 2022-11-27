27af3c2c6f0f89a2f25c29df3eb8282fafcfc844
PaulaSousa2014 <110301198+PaulaSousa2014@users.noreply.github.com>
2022-11-27 10:02:04 +0100

    Abrir plantilla + enunciado programa

---
+++ src/main.c
@@ -0,0 +1,30 @@
+/*	Autor: Paula martins de Sousa		Fecha de creación: 27/11/2022  */
+
+/*
+**	ENUNCIADO DEL PROGRAMA
+**	Se desea  crear una calculadora con las opciones: sumar, restar, multiplicar y 
+dividir, utilizando procedimientos y funciones.
+Mediante un menú (“pintaMenu”), debes preguntar al usuario que operación deseas 
+realizar. El valor que escoja el usuario en el menú, debe estar controlado entre 0 y 3 
+con la función: (“pideNumEntreRango”)
+Posteriormente hay que solicitar los dos operandos para realizar el cálculo mediante 
+un procedimiento llamado (“pideOperadores”). Hay que verificar que el programa 
+no divida por 0, en este caso, debe mostrar un mensaje de error. 
+Para salir del programa hay que poner un 0.
+El main debe mostrar el resultado cada vez que se hace una operación.
+En cada opción (usando un switch) se hará una llamada a las funciones suma, resta, 
+multiplicación o división según la opción escogida.
+El programa finaliza cuando se introduce un 0.
+
+**
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	
+	getch();
+	
+	return 0;
+}
