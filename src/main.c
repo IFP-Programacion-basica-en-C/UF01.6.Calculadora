e8c54d17a3305382b7a38d9954fff26a86b3db26
PaulaSousa2014 <110301198+PaulaSousa2014@users.noreply.github.com>
2022-11-27 11:24:57 +0100

    Sumar

--- src/main.c
+++ src/main.c
@@ -2,17 +2,17 @@
 
 /*
 **	ENUNCIADO DEL PROGRAMA
-**	Se desea  crear una calculadora con las opciones: sumar, restar, multiplicar y 
+**	Se desea  crear una calculadora con las opciones: sumar, restar, multiplicar y
 dividir, utilizando procedimientos y funciones.
-Mediante un menú (“pintaMenu”), debes preguntar al usuario que operación deseas 
-realizar. El valor que escoja el usuario en el menú, debe estar controlado entre 0 y 3 
+Mediante un menú (“pintaMenu”), debes preguntar al usuario que operación deseas
+realizar. El valor que escoja el usuario en el menú, debe estar controlado entre 0 y 3
 con la función: (“pideNumEntreRango”)
-Posteriormente hay que solicitar los dos operandos para realizar el cálculo mediante 
-un procedimiento llamado (“pideOperadores”). Hay que verificar que el programa 
-no divida por 0, en este caso, debe mostrar un mensaje de error. 
+Posteriormente hay que solicitar los dos operandos para realizar el cálculo mediante
+un procedimiento llamado (“pideOperadores”). Hay que verificar que el programa
+no divida por 0, en este caso, debe mostrar un mensaje de error.
 Para salir del programa hay que poner un 0.
 El main debe mostrar el resultado cada vez que se hace una operación.
-En cada opción (usando un switch) se hará una llamada a las funciones suma, resta, 
+En cada opción (usando un switch) se hará una llamada a las funciones suma, resta,
 multiplicación o división según la opción escogida.
 El programa finaliza cuando se introduce un 0.
 
@@ -22,34 +22,46 @@ El programa finaliza cuando se introduce un 0.
 #include <stdio.h>
 void pintaMenu();
 int pideNumEntreRango(int, int, int *);
-void pideOperadores(int, int);
-int suma(int, int);
-int resta(int, int);
-int multiplicacion(int, int);
-int division(int, int);
+void pideOperadores(int *, int *);
+// int suma(int, int);
+// int resta(int, int);
+// int multiplicacion(int, int);
+// int division(int, int);
 
-int main(){
-	int num=1;
+int main()
+{
+	int num = 1;
+	int operador1, operador2;
 
-	printf("BIENVENIDOS AL PROGRAMA CALCULADORA:\n");
-	pintaMenu();
-	pideNumEntreRango(0,4, &num);
-	if(num=0)
+	printf("BIENVENIDOS AL PROGRAMA CALCULADORA:\n\n");
+
+	do
 	{
-		printf("Has salido de la calculadora.");
-	}
-	else{
+		pintaMenu();
+		pideNumEntreRango(0, 4, &num);
 
-	}
+		switch (num)
+		{
+		case 0:
+			printf("Has salido de la calculadora.");
+			break;
 
-	
+		case 1:
+			pideOperadores(&operador1, &operador2);
+			int resultado = operador1 + operador2;
+			printf("\n%d + %d = %d\n", operador1, operador2, resultado);
+			break;
+		case 2:
+		}
+	} while (num != 0);
+
 	getch();
-	
+
 	return 0;
 }
 void pintaMenu()
 {
-printf("Seleccione la operacion que desea realizar:\n0.Salir\n1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir\n");
+	printf("\n\nSeleccione la operacion que desea realizar:\n0.Salir\n1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir\n");
 }
 pideNumEntreRango(int min, int max, int *num)
 {
@@ -57,11 +69,16 @@ pideNumEntreRango(int min, int max, int *num)
 	do
 	{
 		scanf("%d", &*num);
-		printf("Has seleccionado la opcion %d", *num);
+		// printf("\nHas seleccionado la opcion %d", *num);
 	} while (*num <= min || *num > max);
-	
-
-	
 
 	return num;
+}
+void pideOperadores(int *operador1, int *operador2)
+{
+
+	printf("\nIntroduce el operador 1: ");
+	scanf("%d", &*operador1);
+	printf("\nIntroduce el operador 2: ");
+	scanf("%d", &*operador2);
 }
